3fce207addda75f3bd86b07d7829ab11afdf4154
Duk3nd3 <hjmv1982@outlook.com.ar>
2021-10-18 22:55:52 -0300

    Damos estilo basico a la página y agregamos acciones sobre botones nuevos

--- js/productos.js
+++ js/productos.js
@@ -1,9 +1,7 @@
-//PRIMERO CLASES, DESPUES OBJETOS A CREAR DE ESA CLASE, LAS CONSTANTES (FUNCIONES), LUEGO LAS DECLARACIONES DE LAS FUNCIONES Y AL FINAL LAS INVOCACIONES A LAS FUNCIONES
-
 //!CLASE
 class Productos {
 
-    //*CONSTRUCTOR y sus Propiedades
+    //*CONSTRUCTOR Y SUS PROPIEDADES
     constructor(id, video, procesador, memoria, valor, nombre) {
 
         this.id = id
@@ -17,69 +15,95 @@ class Productos {
 
     //*METODO
     mensaje() {
-        
-        console.log("COMPRA REALIZADA CON EXITO" + "\n" + "Placa de video: " + this.video + "\n" + "Procesador: " + this.procesador + "\n" + "Memoria RAM: " + this.memoria + ".");
-        
+
+        console.log("COMPRA REALIZADA CON EXITO" + "\n" + "Placa de video: " + this.video + "\n" + "Procesador: " + this.procesador + "\n" + "Memoria RAM: " + this.memoria + "Precio: " + this.valor + "\n" + "Marca: " + this.nombre + ".");
+
     }
-    
+
 };
 
-//!OBJETOS DE LA CLASE
+//!OBJETOS DE LA CLASE PRODUCTOS
+const combo1 = new Productos(1, "1060", "i3", "DDR3", 130000, "GIGABYTE");
+const combo2 = new Productos(2, "1070", "i5", "DDR4", 210000, "MSI");
+const combo3 = new Productos(3, "1080", "i7", "DDR5", 250000, "EVGA");
 
-const combo1 = new Productos(1, "1060", "i3", "DDR3", 130000, "DXTRUCHA");
-const combo2 = new Productos(2, "1070", "i5", "DDR4", 210000, "GTAMD");
-const combo3 = new Productos(3, "1080", "i7", "DDR5", 250000, "GTXNvidia");
-
-//!FUNCION 
+//!FUNCION 'CONTROLAR'
 const controlar = () => {
-    
+
     let control = true;
     let nombre = prompt("Por favor digita tu nombre: ");
-    
+
     do {
 
         if (nombre == "") {
-        
+
             nombre = prompt("Digita un nombre de usuario correcto")
             control = false;
-            
+
         } else {
-            
+
             control = true;
 
         }
 
     } while (control == false)
 
-        return nombre;
+    return nombre;
 
 }
 
-//!IMPRIMOS NOMBRE USUARIO INVOCANDO A LA FUNCION CONTROLAR
-const saludar = (nombre) => {alert(`Hola ${nombre}`)};
+//!IMPRIMOS NOMBRE USUARIO INVOCANDO A LA FUNCION 'CONTROLAR'
+const saludar = (nombre) => {
+    alert(`Hola ${nombre}`)
+};
 
-//!ARRAY 
-const mochilaCombos = [combo1, combo2, combo3];
+//!ORDENAMOS POR ID LOS PODUCTOS CON EL METODO 'SORT'
+function ordenarAsc() {
 
+
+    const menor = [combo2.id, combo1.id, combo3.id];
+    menor.sort(function(a, b) {return a-b});
+    console.log(menor);
+
+}
+
+function ordenarDesc() {
+
+    const mayor = [combo1.id, combo3.id, combo2.id];
+    mayor.sort(function(a, b) {return b - a});
+    console.log(mayor);
+
+}
+
+//!ARRAY BUNDLE 'COMBOS'
+const bundle = [combo1, combo2, combo3];
+
+//*SOLICITAMOS AL USUARIO EL INGRESO DEL PRODUCTO ADQUIRIDO
 const comunicado = () => {
-    
-    let combos = parseInt(prompt("Ingrese el combo tech escogido (1, 2 o 3: "));
 
-    let respuestaCombo = mochilaCombos.find( combo => combo.id === combos )
+    let combos = parseInt(prompt("Ingrese el producto tech escogido (1, 2 o 3: "))
 
-    alert(`Usted eligio ${respuestaCombo.nombre}`)
-        
+    let respuestaCombo = bundle.find(combo => combo.id === combos)
+
+    alert(`Usted eligio la marca ${respuestaCombo.nombre}`)
+    alert('¡Muchas gracias por comprar en TECH-STORE!')
+
 };
 
 //!AQUI COMENZAMOS CON LA EJECUCION DEL CODIGO LLAMANDO A LAS FUNCIONES
 
-//!OBJETO GLOBAL WINDOWS ESPERA A QUE CARGUE EL HTML Y ESTILOS PARA LUEGO EJECUTAR LO QUE ESTA DENTRO 
-window.onload = function() {
-   
+/* OBJETO GLOBAL WINDOWS ESPERA A QUE CARGUE EL HTML Y ESTILOS PARA LUEGO EJECUTAR LO QUE ESTA DENTRO  */
+window.onload = function () {
+
     let nombreUsuario = controlar();
-    
+
     saludar(nombreUsuario);
-    
+
     comunicado();
-    
+
+    const accionBotonAsc = document.querySelector('#botonOrdenarAsc').addEventListener('click', ordenarAsc);
+
+    const accionBotonDesc = document.querySelector('#botonOrdenarDesc').addEventListener('click', ordenarDesc);
+
 };
+
